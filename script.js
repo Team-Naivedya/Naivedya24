3d21f8a0412dba1971cf5ab443d53480f6e86495
Team-Naivedya <159447230+Team-Naivedya@users.noreply.github.com>
2024-03-06 17:52:34 +0530

    third commit2

--- script.js
+++ script.js
@@ -65,4 +65,15 @@ $(".step").click(function () {
     });
   });
   //===========================================================================================
-  
+  $(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+      items: 1,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 2000,
+      autoplayHoverPause: true,
+      nav: true,
+      dots: true,
+      navText: ["< Prev", "Next >"],
+    });
+  });
